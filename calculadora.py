c03bdc68ee2839c5396eebf951e8e233a4fdf405
Joao Silva <goiaba679@gmail.coom>
2026-07-05 01:15:04 -0300

    Adiciona exponenciaçao e resto da divisão

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+# Mini Calculadora
+# Aluno: Joao Pedro
+
+a = 9
+b = 2
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+exponenciacao = a ** b
+resto = a % b
+
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {subtracao}")
+print(f"{a} * {b} = {multiplicacao}")
+print(f"{a} / {b} = {divisao}")
+print(f"{a} ** {b} = {exponenciacao}")
+print(f"{a} % {b} = {resto}")
